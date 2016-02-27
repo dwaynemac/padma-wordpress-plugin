be333085da558dee2ef5cf53365b0ec26c861c47
Dwayne Macgowan <dwaynemac@gmail.com>
2016-02-27 20:39:51 +0000

    require

--- padma-wordpress-plugin.php
+++ padma-wordpress-plugin.php
@@ -16,6 +16,7 @@ AuthorURI: http://github.com/dwaynemac
 
 define( 'PADMA_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
+require_once( PADMA_PLUGIN_DIR . 'includes/activation-deactivation.php' );
 require_once( PADMA_PLUGIN_DIR . 'includes/options-page.php' );
 require_once( PADMA_PLUGIN_DIR . 'includes/post-to-padma.php' );
 require_once( PADMA_PLUGIN_DIR . 'includes/hook-into-thriveleads.php' );
