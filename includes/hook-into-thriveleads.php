78c73127fe9e35fceb5182ddabe487734897b14b
Dwayne Macgowan <dwaynemac@gmail.com>
2016-03-01 01:07:12 +0000

    this action has only one argument

--- includes/hook-into-thriveleads.php
+++ includes/hook-into-thriveleads.php
@@ -1,9 +1,9 @@
 <?php
 
 // when ThriveLeads form is submitted we'll run our function
-add_action("tcb_api_form_submit", "padma_forward_post_to_padma", 10, 5);
+add_action("tcb_api_form_submit", "padma_forward_post_to_padma", 10, 1);
 
-function padma_forward_post_to_padma($main, $form_type, $variation, $active_test_id, $data, $post_data) {
+function padma_forward_post_to_padma($post_data) {
   $post_data = padma_filter_tve_data($post_data);
   padma_post_form($post_data);
 }
