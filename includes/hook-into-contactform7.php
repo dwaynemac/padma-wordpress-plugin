ec9e98d556b402389644cbe82937d13290d4ad55
Dwayne Macgowan <dwaynemac@gmail.com>
2016-03-01 15:49:29 +0000

    support old contact form 7

--- includes/hook-into-contactform7.php
+++ includes/hook-into-contactform7.php
@@ -1,18 +1,31 @@
 <?php
 
-// wpcf7_submit available since ContacForm7 4.1.2, tested with 4.4
-add_action("wpcf7_submit", "padma_forward_cf7_to_padma", 10, 2);
+if((WPCF7_VERSION !== null) && (floatval(WPCF7_VERSION)>=4.1)){
+  padma_log("detected contactform >= 4.1");
 
-function padma_forward_cf7_to_padma($form,$result) {
-  // TODO has spam been filtered already?
-  // TODO has form been validated already?
-  $submission = WPCF7_Submission::get_instance();
-  if ( $submission ) {
-    $posted_data = $submission->get_posted_data();
+  // wpcf7_submit available since ContacForm7 4.1.2, tested with 4.4
+  add_action("wpcf7_submit", "padma_forward_cf7_to_padma", 10, 2);
+
+  function padma_forward_cf7_to_padma($form,$result) {
+    // TODO has spam been filtered already?
+    // TODO has form been validated already?
+    $submission = WPCF7_Submission::get_instance();
+    if ( $submission ) {
+      $posted_data = $submission->get_posted_data();
+      $posted_data = apply_filters('padma_filter_form_data', $posted_data);
+      do_action('padma_post_form', $posted_data);
+    }
+  };
+} else if((WPCF7_VERSION !== null) && (floatval(WPCF7_VERSION)<=4.1)){
+  padma_log("detected contactform < 4.1");
+  add_action("wpcf7_mail_sent", "padma_forward_old_cf7_to_padma", 10, 1);
+
+  function padma_forward_old_cf7_to_padma($form) {
+    $posted_data = $form->posted_data;
     $posted_data = apply_filters('padma_filter_form_data', $posted_data);
     do_action('padma_post_form', $posted_data);
-  }
-};
+  };
+}
 
 // removes all _wpcf7* fields from form.
 function padma_filter_cf7_data($data){
